cf4c833cd6ec8b0fecb5e73225f62ee27d48ae58
cameron <hammerbro_97@hotmail.com>
2023-03-18 17:18:09 -0600

    arrays

---
+++ js/app.js
@@ -0,0 +1,82 @@
+
+tweeters = [
+
+{
+tweet: `unga bunga`,
+username: `john`,
+created_at: `6-10-2021`,
+age: 30
+},
+{
+tweet: `i ate cheese`,
+username: `ronald`,
+created_at: `3-20-2011`,
+age: 20
+},
+
+{
+tweet: `star wars bad`,
+username: `angel291`,
+created_at: `4-23-2022`,
+age: 10
+},
+{
+tweet: `I love harry potter`,
+username: `stupidguy300`,
+created_at: `1-18-2071`,
+age: 60
+},
+{
+tweet: `elon musk for president`,
+username: `Jillian`,
+created_at: `11-5-2016`,
+age: 23
+},
+{
+tweet: `I'll be back -John Sheppard`,
+username: `adrian3213`,
+created_at: `8-13-2023`,
+age: 43
+},
+{
+tweet: `ok`,
+username: `alex`,
+created_at: `3-18-2023`,
+age: 27
+},
+{
+tweet: `i dont use twitter`,
+username: `personguy21`,
+created_at: `4-4-2018`,
+age: 16
+},
+{
+tweet: `ape together strong`,
+username: `apeguy`,
+created_at: `2-9-2021`,
+age: 19
+},
+{
+tweet: `SWING OF THE MOP`,
+username: `thejanitor`,
+created_at: `9-6-2023`,
+age: 29
+},
+]
+
+
+let counter = 0;
+
+while(counter < tweeters.length){
+    console.log(tweeters[counter]);
+    counter = counter  +1;
+}
+
+console.log('text break');
+
+for(let counter = 0; counter < tweeters.length; counter = counter +1){
+
+console.log(tweeters[counter]);
+
+
+}
